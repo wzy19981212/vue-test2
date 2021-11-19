07d282da27d4aa59cd11f5a644a7489d6de7432d
王舟旸 <1045300366@qq.com>
2021-11-19 21:32:00 +0800

    vue-test2

---
+++ src/components/Add.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-md-4">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label>用户名</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="用户名"
+          v-model="username"
+        />
+      </div>
+      <div class="form-group">
+        <label>评论内容</label>
+        <textarea
+          class="form-control"
+          rows="6"
+          placeholder="评论内容"
+          v-model="content"
+        ></textarea>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="button" class="btn btn-default pull-right" @click="add">
+            提交
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["addcomment"],
+  data() {
+    return {
+      username: "",
+      content: "",
+    };
+  },
+  methods: {
+    add() {
+      let { username, content } = this;
+      if (username.trim() && content.trim()) {
+        let id = Date.now();
+        let obj = {
+          id,
+          username,
+          content,
+        };
+        this.addcomment(obj);
+      } else {
+        alert("你输入的字符有问题");
+      }
+      this.username = "";
+      this.content = "";
+    },
+  },
+};
+</script>
+
+<style  scoped>
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 style="display: none">暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <Item
+        v-for="(comment, index) in coms"
+        :key="comment.id"
+        :comment="comment"
+        :delcomment="delcomment"
+        :index="index"
+      ></Item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+export default {
+  components: {
+    Item,
+  },
+  props: ["coms", "delcomment"],
+};
+</script>
+
+<style scoped>
+.reply {
+  margin-top: 0px;
+}
+</style>
